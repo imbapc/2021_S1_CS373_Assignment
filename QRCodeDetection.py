c03927ec8d962e7a3043b1b63fd2e4d88435683b
hhon669 <danialhong97@gmail.com>
2021-06-08 15:54:24 +1200

    normal code is done

--- QRCodeDetection.py
+++ QRCodeDetection.py
@@ -1,8 +1,12 @@
 
 from matplotlib import pyplot
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, Polygon
+from pyzbar.pyzbar import decode
+import os
 
 import imageIO.png
+
+
 
 #This is the queue class for Connected Conponent
 class Queue:
@@ -26,6 +30,48 @@ def createInitializedGreyscalePixelArray(image_width, image_height, initValue = 
 
     new_array = [[initValue for x in range(image_width)] for y in range(image_height)]
     return new_array
+
+
+# This method applies dilation
+def applyDilation(pixel_array, w, h):
+    dilation_array = createInitializedGreyscalePixelArray(w, h)
+    for i in range(h):
+        for j in range(w):
+            if not pixel_array[i][j] == 0:
+                dilation_array[i][j] = 1
+                if not j - 1 < 0:
+                    dilation_array[i][j - 1] = 1
+                if j + 1 < w:
+                    dilation_array[i][j + 1] = 1
+                if not i - 1 < 0:
+                    dilation_array[i - 1][j] = 1
+                    if not j - 1 < 0:
+                        dilation_array[i - 1][j - 1] = 1
+                    if  j + 1 < w:
+                        dilation_array[i - 1][j + 1] = 1
+                if i + 1 < h:
+                    dilation_array[i + 1][j] = 1
+                    if not j - 1 < 0:
+                        dilation_array[i + 1][j - 1] = 1
+                    if j + 1 < w:
+                        dilation_array[i + 1][j + 1] = 1
+
+    return dilation_array
+
+
+# This method applies erosion
+def applyErosion(pixel_array, w, h):
+    kernel_array = createInitializedGreyscalePixelArray(w, h)
+    for i in range(1, h - 1):
+        for j in range(1, w - 1):
+            if pixel_array[i - 1][j - 1] == 0 or pixel_array[i - 1][j] == 0 or pixel_array[i - 1][j + 1] == 0 \
+                    or pixel_array[i][j - 1] == 0 or pixel_array[i][j] == 0 or pixel_array[i][j + 1] == 0 \
+                    or pixel_array[i + 1][j - 1] == 0 or pixel_array[i + 1][j] == 0 or pixel_array[i + 1][j + 1] == 0:
+                kernel_array[i][j] = 0
+            else:
+                kernel_array[i][j] = 1
+
+    return kernel_array
 
 
 # this function reads an RGB color png file and returns width, height, as well as pixel arrays for r,g,b
@@ -67,6 +113,7 @@ def readRGBImageToSeparatePixelArrays(input_filename):
 
     return (image_width, image_height, pixel_array_r, pixel_array_g, pixel_array_b)
 
+
 # This method packs together three individual pixel arrays for r, g and b values into a single array that is fit for
 # use in matplotlib's imshow method
 def prepareRGBImageForImshowFromIndividualArrays(r,g,b,w,h):
@@ -91,6 +138,7 @@ def writeGreyscalePixelArraytoPNG(output_filename, pixel_array, image_width, ima
     writer.write(file, pixel_array)
     file.close()
 
+
 # This method takes image width, height and pixel array for red, green and blue and write a greyscale pixel array.
 def getGreyscalePixelArrayfromPixelArray(w,h,r,g,b):
     result_array = createInitializedGreyscalePixelArray(w,h)
@@ -101,6 +149,7 @@ def getGreyscalePixelArrayfromPixelArray(w,h,r,g,b):
     print(result_array)
     return result_array
 
+
 # This method takes image width, height and the greyscale and calculate the horizontal edges of the image
 def getHorizontalEdges(w,h,g):
     result_array = createInitializedGreyscalePixelArray(w, h)
@@ -110,6 +159,7 @@ def getHorizontalEdges(w,h,g):
 
     return result_array
 
+
 # This method takes image width, height and the greyscale and calculate the vertical edges of the image
 def getVerticalEdges(w,h,g):
     result_array = createInitializedGreyscalePixelArray(w, h)
@@ -118,6 +168,7 @@ def getVerticalEdges(w,h,g):
             result_array[i][j] = round((g[i-1][j+1]*1+g[i][j+1]*2+g[i+1][j+1]*1-g[i-1][j-1]*1-g[i][j-1]*2-g[i+1][j-1]*1)/8)
 
     return result_array
+
 
 # This method takes image width, height, and both horizontal and vertical edges, return the gradient magnitude of the
 # image
@@ -129,6 +180,7 @@ def getGradientMagnitude(w,h,horizontal,vertical):
 
     return result_array
 
+
 # This method takes image width, height and the gradient magnitude and apply a Gaussian smooth to it
 def applyGaussianSmooth(w,h,g):
     result_array = createInitializedGreyscalePixelArray(w,h)
@@ -139,6 +191,7 @@ def applyGaussianSmooth(w,h,g):
 #            result_array[i][j] = round((g[i-1][j-1]+g[i-1][j]+g[i-1][j+1]+g[i][j-1]+g[i][j]+g[i][j+1]+g[i+1][j-1]+g[i+1][j]+g[i+1][j+1])/9)
 
     return result_array
+
 
 # This method takes image width, height and a greyscale array and apply thresholding operation with thresholding number 1
 # and return the result array
@@ -153,7 +206,17 @@ def applyThresholdingOperation(w,h,g,t=20):
 
     return result_array
 
-#This method takes image width, height and a binary greyscalle array and get all the connnected objects.
+
+# This method applies a morphological closing operation
+def applyClosing(pixel_array, w, h):
+    for i in range(10):
+        dilation = applyDilation(pixel_array, w, h)
+    for i in range(10):
+        erosion = applyErosion(dilation, w, h)
+    return erosion
+
+
+# This method takes image width, height and a binary greyscale array and get all the connnected objects.
 def computeConnectedComponentLabeling(pixel_array, w, h):
     result_array = createInitializedGreyscalePixelArray(w, h)
     visited_pixel = createInitializedGreyscalePixelArray(w, h)
@@ -197,6 +260,28 @@ def computeConnectedComponentLabeling(pixel_array, w, h):
                 key = key + 1
     return (result_array, result_dict)
 
+
+# This method detects four vortexes of possible object from a binary array
+def computeVortexes(pixel_array, w, h):
+    min_h = h
+    min_w = w
+    max_h = 0
+    max_w = 0
+
+    for i in range(h):
+        for j in range(w):
+            if pixel_array[i][j] == 1:
+                if min_w > j:
+                    min_w = j
+                if max_w < j:
+                    max_w = j
+                if min_h > i:
+                    min_h = i
+                if max_h < i:
+                    max_h = i
+    return (min_w, min_h, max_w, max_h)
+
+
 #This method defines the biggest object in the picture
 def computeMainObject(pixel_array, array_dict, w, h):
     max = 0
@@ -211,31 +296,38 @@ def computeMainObject(pixel_array, array_dict, w, h):
             if pixel_array[i][j] != num:
                 result_array[i][j] = 0
             else:
-                result_array[i][j] = 255
+                result_array[i][j] = 1
     return result_array
 
-#This method record the index of main object edges
+
+#This method return the edge of pixel_array
 def computeMainObjectEdge(pixel_array, w, h):
-    min_w = w
-    max_w = 0
-    min_h = h
-    max_h = 0
+    result_array = createInitializedGreyscalePixelArray(w, h)
     for i in range(h):
         for j in range(w):
-            if pixel_array[i][j] == 255:
-                if i < min_h:
-                    min_h = i
-                if i > max_h:
-                    max_h = i
-                if j < min_w:
-                    min_w = j
-                if j > max_w:
-                    max_w = j
+            if not pixel_array[i][j] == 0:
+                if not j - 1 < 0:
+                    result_array[i][j - 1] = 1 - pixel_array[i][j - 1]
+                if j + 1 < w:
+                    result_array[i][j + 1] = 1 - pixel_array[i][j + 1]
+                if not i - 1 < 0:
+                    result_array[i - 1][j] = 1 - pixel_array[i - 1][j]
+                    if not j - 1 < 0:
+                        result_array[i - 1][j - 1] = 1 - pixel_array[i - 1][j - 1]
+                    if j + 1 < w:
+                        result_array[i - 1][j + 1] = 1 - pixel_array[i - 1][j + 1]
+                if i + 1 < h:
+                    result_array[i + 1][j] = 1 - pixel_array[i + 1][j]
+                    if not j - 1 < 0:
+                        result_array[i + 1][j - 1] = 1 - pixel_array[i + 1][j - 1]
+                    if j + 1 < w:
+                        result_array[i + 1][j + 1] = 1 - pixel_array[i + 1][j + 1]
 
-    return (min_w, min_h, max_w-min_w, max_h-min_h)
+    return result_array
 
 
 def main():
+    os.add_dll_directory("D:/project/venv/lib/site-packages/pyzbar")
     filename = "./images/covid19QRCode/poster1small.png"
 
     # we read in the png file, and receive three pixel arrays for red, green and blue components, respectively
@@ -245,7 +337,7 @@ def main():
     pyplot.imshow(prepareRGBImageForImshowFromIndividualArrays(px_array_r, px_array_g, px_array_b, image_width, image_height))
 
     greyscale_array = getGreyscalePixelArrayfromPixelArray(image_width, image_height, px_array_r, px_array_g, px_array_r)
-    writeGreyscalePixelArraytoPNG("poster1small_grey.png", greyscale_array, image_width, image_height)
+    #writeGreyscalePixelArraytoPNG("poster1smallrotated.png", greyscale_array, image_width, image_height)
     horizontal_edge = getHorizontalEdges(image_width,image_height,greyscale_array)
     vertical_edge = getVerticalEdges(image_width, image_height, greyscale_array)
     gradient_magnitude = getGradientMagnitude(image_width,image_height,horizontal_edge,vertical_edge)
@@ -253,18 +345,22 @@ def main():
     for i in range(10):
         gaussian_smooth = applyGaussianSmooth(image_width,image_height, gaussian_smooth)
     thresholding_operation = applyThresholdingOperation(image_width,image_height,gaussian_smooth)
-    (connected_array, array_dict) = computeConnectedComponentLabeling(thresholding_operation, image_width, image_height)
+    closing_array = applyClosing(thresholding_operation, image_width, image_height)
+    (connected_array, array_dict) = computeConnectedComponentLabeling(closing_array, image_width, image_height)
     main_object = computeMainObject(connected_array,array_dict,image_width,image_height)
-    (min_w, min_h, width, height) = computeMainObjectEdge(main_object, image_width, image_height)
+    (min_w, min_h, max_w, max_h) = computeVortexes(main_object, image_width, image_height)
+    main_object_edge = computeMainObjectEdge(main_object, image_width, image_height)
 
-    #pyplot.imshow(main_object, cmap='gray')
+    #pyplot.imshow(main_object_edge, cmap='gray')
+
 
     # get access to the current pyplot figure
     axes = pyplot.gca()
     # create a 70x50 rectangle that starts at location 10,30, with a line width of 3
-    rect = Rectangle( (min_w-3, min_h-3), width+6, height+6, linewidth=3, edgecolor='g', facecolor='none')
+    rect = Rectangle((min_w-3, min_h-3), (max_w-min_w)+6, (max_h-min_h)+6, linewidth=3, edgecolor='g', facecolor='none')
     # paint the rectangle over the current plot
     axes.add_patch(rect)
+    decode(greyscale_array.tobytes(), image_width, image_height)
 
     # plot the current figure
     pyplot.show()
